8ea481fa9f71dd220bcef28fa9ba5a98edd1b734
Manuel Cabral <m.cabral@digisfera.pt>
2014-02-01 15:57:17 +0000

    Refactoring

--- index.js
+++ index.js
@@ -1,15 +1,72 @@
 var fileFun = require('file-fun'),
+    _ = require('lodash'),
+    path = require('path'),
     UglifyJS = require("uglify-js");
 
-exports.sync = function(contents, options) {
-  options = options || {};
-  options.fromString = true;
-
-  var concatted = contents.join(';');
-
-  var minified = UglifyJS.minify(concatted, options).code;
-  return minified
+relativePath = function(origFilePath, filePathToCalculate) {
+  var p = path.relative(path.dirname(origFilePath), filePathToCalculate);
+  return p.split(path.sep).join('/');
 }
-exports.globsToFile = fileFun.sync_globsToFile(exports.sync)
-exports.filesToFile = fileFun.sync_filesToFile(exports.sync)
-exports.async = fileFun.sync_async(exports.sync)
+
+
+function uglifyData(data, options) {
+  options = options || {};
+
+  var compileOptions = _.omit(options, 'sourceMap', 'sourceMapFile', 'inputFileForSourceMap', 'outputFileForSourceMap');
+
+  compileOptions.fromString = true;
+
+  if(options.sourceMapFile) {
+    compileOptions.outSourceMap = relativePath(options.outputFileForSourceMap, options.sourceMapFile);
+    compileOptions.output = compileOptions.output || {};
+    
+    var source_map = UglifyJS.SourceMap({
+        file: relativePath(options.sourceMapFile, options.outputFileForSourceMap),
+        root: relativePath(options.sourceMapFile, path.dirname(options.inputFileForSourceMap))
+        //orig: ORIG_MAP,
+    });
+
+    compileOptions.output.source_map = source_map;
+  }
+
+  var result = UglifyJS.minify(data, compileOptions);
+
+  if(options.sourceMapFile && result.map) {
+    result.code += "\n//# sourceMappingURL="+relativePath(options.outputFileForSourceMap, options.sourceMapFile);
+  }
+
+  return result;
+}
+
+
+module.exports = function(inputFiles, outputFile, options, callback) {
+  if(_.isString(inputFiles)) { inputFiles = [ inputFiles ]; }
+
+  fileFun.readFilesUtf8(inputFiles, function(err, filesData) {
+
+    var data = null;
+    var sourceMapFile = options.sourceMapFile || outputFile + '.map';
+
+    if(filesData.length > 0) {
+      //More than one file, we can't handle sourcemaps in this case yet
+      data = filesData.join(';');
+      options.sourceMap = false;
+    }
+    else {
+      data = filesData[0];
+      options.inputFileForSourceMap = inputFiles[0];
+      options.outputFileForSourceMap = outputFile;
+      options.sourceMapFile = sourceMapFile;
+    }
+
+    result = uglifyData(data, options);
+
+    if(result.map) {
+      fileFun.mkWriteFiles([[ outputFile, result.code ], [sourceMapFile, result.map]], callback);
+    }
+    else {
+      fileFun.mkWriteFile(outputFile, result.code, callback);
+    }
+
+  });
+}
---
+++ test/files/script.js
@@ -0,0 +1,1 @@
+(function thatFun() { console.log("that"); })()
